3eae838e2dc0d5a35a3ca43d289e6e548146d1b0
trebd17-art <trebd17@gmail.com>
2026-06-18 22:25:16 -0400

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-function showScreen(screenId) {
-  document.querySelectorAll(".screen").forEach(screen => {
-    screen.classList.remove("active");
-  });
-  document.getElementById(screenId).classList.add("active");
-}
